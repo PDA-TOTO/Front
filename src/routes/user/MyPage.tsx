73e1da65e0294a1ea62e83f04656525679a70156
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-20 16:10:23 +0900

    feat : stock detail community 추가

--- src/routes/user/MyPage.tsx
+++ src/routes/user/MyPage.tsx
@@ -1,28 +1,61 @@
-import React,{useState} from 'react'
-import { Tabs, Flex, Text, Progress } from '@mantine/core'
+import React, { useState } from "react";
+import { Tabs, Flex, Text, Progress } from "@mantine/core";
 import classes from "../../styles/user/MyPage.module.css";
 
 export default function MyPage() {
-    const [tab,setTab] = useState('마이페이지');
+  const [tab, setTab] = useState("마이페이지");
 
-    
-return (
+  return (
     <Flex direction="column" p="40px" gap="40px">
-        <Tabs defaultValue="first" variant='pills' color="primary.5">
-            <Tabs.List>
-            <Tabs.Tab value="first" fz={"18px"} w="150px" h="50px" fw="600" onClick={()=>{setTab("마이페이지")}}>마이페이지</Tabs.Tab>
-            <Tabs.Tab value="second" fz={"18px"} w="150px" h="50px" fw="600"  onClick={()=>{setTab("거래 내역")}}>거래 내역</Tabs.Tab>
-            </Tabs.List>
-        </Tabs>
-        <Flex bg="block.5"  mih="75vh">
-            <Flex className={classes.profile_Card} direction="column" bg="primary.9" w="300px" h="150px" p="10px" gap="15px">
-                    <Text c="white.5" size="22px" fw="600">hello@naver.com</Text>
-                    <Flex direction="column" gap="5px">
-                        <Text c="white.5" size="18px" fw="600">Silver</Text>
-=                       <Progress color="blue.5" bg="white.5" value={70} animated/>
-                    </Flex>
-            </Flex>
+      <Tabs defaultValue="first" variant="pills" color="primary.5">
+        <Tabs.List>
+          <Tabs.Tab
+            value="first"
+            fz={"18px"}
+            w="150px"
+            h="50px"
+            fw="600"
+            onClick={() => {
+              setTab("마이페이지");
+            }}
+          >
+            마이페이지
+          </Tabs.Tab>
+          <Tabs.Tab
+            value="second"
+            fz={"18px"}
+            w="150px"
+            h="50px"
+            fw="600"
+            onClick={() => {
+              setTab("거래 내역");
+            }}
+          >
+            거래 내역
+          </Tabs.Tab>
+        </Tabs.List>
+      </Tabs>
+      <Flex bg="block.5" mih="75vh">
+        <Flex
+          className={classes.profile_Card}
+          direction="column"
+          bg="primary.9"
+          w="300px"
+          h="150px"
+          p="10px"
+          gap="15px"
+        >
+          <Text c="white.5" size="22px" fw="600">
+            hello@naver.com
+          </Text>
+          <Flex direction="column" gap="5px">
+            <Text c="white.5" size="18px" fw="600">
+              Silver
+            </Text>
+            <Progress color="blue.5" bg="white.5" value={70} animated />
+          </Flex>
         </Flex>
+      </Flex>
     </Flex>
-    );
+  );
 }
