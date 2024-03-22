3d4bef04fd338043e655493a20141fa8bd9e678c
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-22 11:35:34 +0900

    feat : 잠금 tab 추가

--- src/components/stockDetail/StockNav.tsx
+++ src/components/stockDetail/StockNav.tsx
@@ -2,6 +2,11 @@ import { Tabs, Text } from "@mantine/core";
 import StockCommunity from "./community/StockCommunity";
 import StockNews from "./news/StockNews";
 import { useParams } from "react-router-dom";
+import LockInfo from "./common/LockInfo";
+import LockResearch from "../../assets/img/stock/lock/LockResearch.png";
+import LockNews from "../../assets/img/stock/lock/LockNews.png";
+import LockCommunity from "../../assets/img/stock/lock/LockCommunity.png";
+import LockInformation from "../../assets/img/stock/lock/LockInformation.png";
 
 const StockNav: React.FC = () => {
   const { id } = useParams();
@@ -29,7 +34,7 @@ const StockNav: React.FC = () => {
   ];
 
   return (
-    <Tabs color="primary.5" defaultValue="summary">
+    <Tabs color="primary.5" defaultValue="summary" style={{ zIndex: 5 }}>
       <Tabs.List grow justify="space-between">
         {tabs.map((tab) => (
           <Tabs.Tab key={tab.value} value={tab.value}>
@@ -45,7 +50,14 @@ const StockNav: React.FC = () => {
       </Tabs.Panel>
       <Tabs.Panel value="info">fadfafd</Tabs.Panel>
       <Tabs.Panel value="news">{id && <StockNews id={id} />}</Tabs.Panel>
-      <Tabs.Panel value="research">fadfafd</Tabs.Panel>
+      <Tabs.Panel value="research">
+        <LockInfo
+          tabName={"리서치"}
+          btnName1="커뮤니티"
+          btnName2="퀴즈"
+          imgLink={LockInformation}
+        />
+      </Tabs.Panel>
     </Tabs>
   );
 };
---
+++ src/components/stockDetail/common/LockInfo.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import classes from "../../../styles/stock/common/LockInfo.module.css";
+import { Flex, Image } from "@mantine/core";
+import lock from "../../../assets/img/stock/lock/lock.svg";
+
+type Props = {
+  tabName: string;
+  btnName1: string;
+  btnName2: string;
+  imgLink: string;
+};
+
+export default function LockInfo({
+  tabName,
+  btnName1,
+  btnName2,
+  imgLink,
+}: Props) {
+  return (
+    <Flex
+      direction={"column"}
+      justify="center"
+      align="center"
+      className={classes.lock_box}
+    >
+      <Image src={imgLink} className={classes.lock_img_blur} />
+      <Flex
+        direction={"column"}
+        justify={"center"}
+        align={"center"}
+        className={classes.lock_box_2}
+      >
+        <Flex
+          direction={"row"}
+          justify="center"
+          align="center"
+          className={classes.lock_title}
+        >
+          <div className={classes.lock_img}>
+            <Image src={lock} />
+          </div>
+          아직 {tabName} 탭을 이용하실 수 없어요🥲{" "}
+        </Flex>
+        <Flex
+          direction={"column"}
+          align="center"
+          className={classes.lock_content}
+        >
+          <div>
+            투투는 여러분들이 해당 서비스가 왜 필요한지 느꼈으면 좋겠어요
+          </div>
+          <div>아래의 활동을 통해서 투자 지식을 늘리고 해금할 수 있어요!</div>
+        </Flex>
+        <Flex direction={"row"}>
+          <button className={`${classes.lock_btn1} ${classes.lock_btn}`}>
+            {btnName1}
+          </button>
+          <button className={`${classes.lock_btn2} ${classes.lock_btn}`}>
+            {btnName2}
+          </button>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
