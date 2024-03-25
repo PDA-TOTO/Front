19e6b05f12af053fff3cd37afeffeace4a854d5d
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-25 16:13:59 +0900

    feat : stock major api 추가

--- src/lib/apis/stock.tsx
+++ src/lib/apis/stock.tsx
@@ -3,3 +3,7 @@ import { stockInstance } from "./api";
 export async function stockNews(stockId: string) {
   return await stockInstance.get("/" + stockId + "/news");
 }
+
+export async function stockMajors() {
+  return await stockInstance.get("/majors");
+}
