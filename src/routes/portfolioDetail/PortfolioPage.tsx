c568f82687b4af71d8a1accb36b1674f1589bb0c
Dongwon KIm <dongwon000103@gmail.com>
2024-03-27 16:22:43 +0900

    hotfix: hotfix

--- src/routes/portfolioDetail/PortfolioPage.tsx
+++ src/routes/portfolioDetail/PortfolioPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Grid,Switch,Table } from '@mantine/core';
+import { Grid, Switch, Table } from '@mantine/core';
 import '@mantine/charts/styles.css';
 // import { Bar } from 'react-chartjs-2';
 import { BarChart } from '@mantine/charts';
@@ -7,7 +7,6 @@ import { BarChart } from '@mantine/charts';
 import { ScatterChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Scatter, Bar } from 'recharts';
 import { useNavigate } from 'react-router-dom';
 import { getPortNames } from '../../lib/apis/portfolios';
-import cookie from 'react-cookies';
 
 const data01 = [
     { x: 1, y: 2 },
@@ -18,16 +17,16 @@ const data01 = [
 ];
 
 const port = {
-    "items" : [
-        {"stockId" : "005930", "name":"삼성전자", "weight" : 0.5},
-        {"stockId" : "005930", "name":"SK하이닉스", "weight" : 0.3},
-        {"stockId" : "005930", "name":"셀트리온", "weight" : 0.2}
+    items: [
+        { stockId: '005930', name: '삼성전자', weight: 0.5 },
+        { stockId: '005930', name: 'SK하이닉스', weight: 0.3 },
+        { stockId: '005930', name: '셀트리온', weight: 0.2 },
     ],
-    "portName" : "포트1",
+    portName: '포트1',
     // "items" : ["삼성전자","SK하이닉스","셀트리온"],
     // "weight" : [0.5, 0.25, 0.25],
-    "portId" : "1234"
-}
+    portId: '1234',
+};
 
 // const portNames = ["포트1","포트폴리오2","프로포폴3"]
 
@@ -53,86 +52,88 @@ const ScatterChartComponent: React.FC = () => {
         </ScatterChart>
     );
 };
-function portSelectClicked(){
-    console.log(1)
+function portSelectClicked() {
+    console.log(1);
 }
 
 function Demo() {
-    const elements :{ [key : string] : number} =  { m1 : 10, m3 : -20, m6 : -30, m12 : 40}
+    const elements: { [key: string]: number } = { m1: 10, m3: -20, m6: -30, m12: 40 };
     const rows = (
-      <Table.Tr>
-        {Object.keys(elements).map((key) => (
-            <Table.Td style={elements[key]>0 ? {color:"red"}: {color:"blue"}}>{elements[key]} %</Table.Td>
-        ))}
-      </Table.Tr>
+        <Table.Tr>
+            {Object.keys(elements).map((key) => (
+                <Table.Td style={elements[key] > 0 ? { color: 'red' } : { color: 'blue' }}>{elements[key]} %</Table.Td>
+            ))}
+        </Table.Tr>
     );
-  
-    return (
-      <Table>
-        <Table.Thead>
-          <Table.Tr>
-            <Table.Th>1개월</Table.Th>
-            <Table.Th>3개월</Table.Th>
-            <Table.Th>6개월</Table.Th>
-            <Table.Th>12개월</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
 
-        <Table.Tbody>{rows}</Table.Tbody>
-        
-      </Table>
+    return (
+        <Table>
+            <Table.Thead>
+                <Table.Tr>
+                    <Table.Th>1개월</Table.Th>
+                    <Table.Th>3개월</Table.Th>
+                    <Table.Th>6개월</Table.Th>
+                    <Table.Th>12개월</Table.Th>
+                </Table.Tr>
+            </Table.Thead>
+
+            <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
     );
 }
 
-const data = [
-    { month: 'January', Smartphones: 1200, Laptops: 900, Tablets: 200 }
-]
-  
+const data = [{ month: 'January', Smartphones: 1200, Laptops: 900, Tablets: 200 }];
 
 const PortfolioPage: React.FC = () => {
-    const navigate = useNavigate()
-    const portNames = useState<string>([])
-    
-    useEffect(()=>{
-        const userCookie = cookie.load('userid')
-        const names = getPortNames(userCookie);
-    },[])
+    const navigate = useNavigate();
+    const [portNames, setPortNames] = useState<string[]>([]);
 
     return (
-        <Grid grow justify="space-between" px={{ base: 72 }} pt={34} style={{display:"flex"}}>
+        <Grid grow justify="space-between" px={{ base: 72 }} pt={34} style={{ display: 'flex' }}>
             <Grid.Col span={4}>
-                <h2>
-                    포트폴리오 지표 분석
-                </h2>
+                <h2>포트폴리오 지표 분석</h2>
                 <div style={{ width: '100%', height: 300 }}>
                     <ScatterChartComponent />
                 </div>
-                
-                <div style={{fontWeight : 'bold' ,display : "flex"}}>
-                    <div>
-                        포트폴리오
-                    </div>
-                    <select style={{marginLeft:"10px"}}>
-                        {portNames.map((elem,idx)=>{return <option value={idx}>{elem}</option>})}
+
+                <div style={{ fontWeight: 'bold', display: 'flex' }}>
+                    <div>포트폴리오</div>
+                    <select style={{ marginLeft: '10px' }}>
+                        {portNames.map((elem: string, idx: number) => {
+                            return <option value={idx}>{elem}</option>;
+                        })}
                     </select>
-                    <button style={{marginLeft:"10px"}} onClick={()=>portSelectClicked()}>
+                    <button style={{ marginLeft: '10px' }} onClick={() => portSelectClicked()}>
                         보기
                     </button>
-                    <button style={{marginLeft:"10px"}} onClick={()=>{portSelectClicked(); navigate("create")}}>
+                    <button
+                        style={{ marginLeft: '10px' }}
+                        onClick={() => {
+                            portSelectClicked();
+                            navigate('create');
+                        }}
+                    >
                         포트폴리오 만들기
                     </button>
                 </div>
 
-                <div style={{display: "flex", alignItems: "center"}}>
-                    <h3 style={{fontWeight : 'bold' ,display : "flex"}}>투자구성종목</h3>
-                    <div style={{marginLeft:"25px", fontSize:"12px"}}>자세히보기</div>
-                    <Switch style={{marginLeft:"10px"}}/>
-                    <button style={{fontSize:"10px",marginLeft:"10px"}} onClick={()=>{portSelectClicked(); navigate("edit",{ state : port})}}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <h3 style={{ fontWeight: 'bold', display: 'flex' }}>투자구성종목</h3>
+                    <div style={{ marginLeft: '25px', fontSize: '12px' }}>자세히보기</div>
+                    <Switch style={{ marginLeft: '10px' }} />
+                    <button
+                        style={{ fontSize: '10px', marginLeft: '10px' }}
+                        onClick={() => {
+                            portSelectClicked();
+                            navigate('edit', { state: port });
+                        }}
+                    >
                         편집
                     </button>
                 </div>
                 {/* <Bar options={options} data={data}></Bar> */}
-                <BarChart h={300}
+                <BarChart
+                    h={300}
                     data={data}
                     dataKey="month"
                     // xScale={{ type: 'linear' }}
@@ -143,7 +144,7 @@ const PortfolioPage: React.FC = () => {
                         { name: 'Laptops', color: 'blue.6' },
                         { name: 'Tablets', color: 'teal.6' },
                     ]}
-                />              
+                />
                 <h3>
                     기간별 수익률
                     {Demo()}
