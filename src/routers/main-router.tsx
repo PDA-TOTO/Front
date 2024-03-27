449624f3d7a56541f07fffba1c2e51908d37ed30
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-27 14:31:58 +0900

    chore : 오타 수정

--- src/routers/main-router.tsx
+++ src/routers/main-router.tsx
@@ -1,112 +1,112 @@
-import { createBrowserRouter } from 'react-router-dom';
-import MainLayout from '../routes/MainLayout';
-import LoginPage from '../routes/user/LoginPage';
-import SignupPage from '../routes/user/SignupPage';
-import StockDetailPage from '../routes/stockDetail/StockDetailPage';
-import PortfolioPage from '../routes/portfolioDetail/PortfolioPage';
-import PortfolioEdit from '../routes/portfolioDetail/PortfolioEdit';
-import QuizTestPage from '../routes/quiz/QuizTestPage';
-import QuizMainPage from '../routes/quiz/QuizMainPage';
-import StockPage from '../routes/stock/StockPage';
-import CommunityPage from '../routes/stockDetail/CommunityPage';
-import MyPage from '../routes/user/MyPage';
-import { Provider } from 'react-redux';
-import stockDetailStore from '../store/stockDetailStore';
-import MainRenewPage from '../routes/MainRenewPage';
-import NotFoundPage from '../routes/NotFoundPage';=
-import TendencyTestPage from '../routes/quiz/TendencyTestPage';
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../routes/MainLayout";
+import LoginPage from "../routes/user/LoginPage";
+import SignupPage from "../routes/user/SignupPage";
+import StockDetailPage from "../routes/stockDetail/StockDetailPage";
+import PortfolioPage from "../routes/portfolioDetail/PortfolioPage";
+import PortfolioEdit from "../routes/portfolioDetail/PortfolioEdit";
+import QuizTestPage from "../routes/quiz/QuizTestPage";
+import QuizMainPage from "../routes/quiz/QuizMainPage";
+import StockPage from "../routes/stock/StockPage";
+import CommunityPage from "../routes/stockDetail/CommunityPage";
+import MyPage from "../routes/user/MyPage";
+import { Provider } from "react-redux";
+import stockDetailStore from "../store/stockDetailStore";
+import MainRenewPage from "../routes/MainRenewPage";
+import NotFoundPage from "../routes/NotFoundPage";
+import TendencyTestPage from "../routes/quiz/TendencyTestPage";
 
 export const mainRouter = [
-    {
-        path: '',
-        element: <MainLayout />,
+  {
+    path: "",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "",
+        element: <MainRenewPage />,
+        index: true,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+        index: true,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+        index: true,
+      },
+      {
+        path: "/my",
+        element: <MyPage />,
+        index: true,
+      },
+      {
+        path: "/quiz",
+        element: <QuizMainPage />,
+        index: true,
+      },
+      {
+        path: "/stocks",
+        element: <StockPage />,
+        index: true,
+      },
+      {
+        path: "/portfolio",
         children: [
-            {
-                path: '',
-                element: <MainRenewPage />,
-                index: true,
-            },
-            {
-                path: '/login',
-                element: <LoginPage />,
-                index: true,
-            },
-            {
-                path: '/signup',
-                element: <SignupPage />,
-                index: true,
-            },
-            {
-                path: '/my',
-                element: <MyPage />,
-                index: true,
-            },
-            {
-                path: '/quiz',
-                element: <QuizMainPage />,
-                index: true,
-            },
-            {
-                path: '/stocks',
-                element: <StockPage />,
-                index: true,
-            },
-            {
-                path: '/portfolio',
-                children: [
-                    {
-                        path: '',
-                        element: <PortfolioPage />,
-                        index: true,
-                    },
-                    {
-                        path: 'edit',
-                        element: <PortfolioEdit />,
-                        index: true,
-                    },
-                    {
-                        path: 'create',
-                        element: <PortfolioEdit />,
-                        // index: true,
-                    },
-                ],
-            },
-            {
-                path: '/stocks',
-                children: [
-                    {
-                        path: ':id',
-                        element: (
-                            <Provider store={stockDetailStore}>
-                                <StockDetailPage />
-                            </Provider>
-                        ),
-                        index: true,
-                    },
-                    {
-                        path: ':id/community',
-                        element: <CommunityPage />,
-                        index: true,
-                    },
-                ],
-            },
-            {
-                path: '/*',
-                element: <NotFoundPage />,
-                index: true,
-            },
+          {
+            path: "",
+            element: <PortfolioPage />,
+            index: true,
+          },
+          {
+            path: "edit",
+            element: <PortfolioEdit />,
+            index: true,
+          },
+          {
+            path: "create",
+            element: <PortfolioEdit />,
+            // index: true,
+          },
         ],
-    },
-    {
-        path: '/quiztest',
-        element: <QuizTestPage />,
+      },
+      {
+        path: "/stocks",
+        children: [
+          {
+            path: ":id",
+            element: (
+              <Provider store={stockDetailStore}>
+                <StockDetailPage />
+              </Provider>
+            ),
+            index: true,
+          },
+          {
+            path: ":id/community",
+            element: <CommunityPage />,
+            index: true,
+          },
+        ],
+      },
+      {
+        path: "/*",
+        element: <NotFoundPage />,
         index: true,
-    },
-    {
-        path: '/tendencytest',
-        element: <TendencyTestPage />,
-        index: true,
-    }
+      },
+    ],
+  },
+  {
+    path: "/quiztest",
+    element: <QuizTestPage />,
+    index: true,
+  },
+  {
+    path: "/tendencytest",
+    element: <TendencyTestPage />,
+    index: true,
+  },
 ];
 
 const router = createBrowserRouter(mainRouter);
