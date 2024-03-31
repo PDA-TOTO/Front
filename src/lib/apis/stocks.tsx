ffcd9046395aa41f882a7b1194577a51539bdbc4
parkjineon <truewords0113@naver.com>
2024-03-31 13:47:07 +0900

    Fix: mypage UI

--- src/lib/apis/stocks.tsx
+++ src/lib/apis/stocks.tsx
@@ -1,11 +1,19 @@
 import { stockInstance } from "./api";
 
-export async function getAllStockNames(){
-    const stocks = stockInstance.get("/");
-    console.log(stocks)
-    // stocks.map((elem)=>{
+export async function getAllStockNames() {
+  const stocks = stockInstance.get("/");
+  console.log(stocks);
+  // stocks.map((elem)=>{
 
-    // })
-    return await stockInstance.get("/");
+  // })
+  return await stockInstance.get("/");
+}
 
+export async function getStockTransaction(page: number, size: number) {
+  return await stockInstance.get("transactions?page=1&size=2", {
+    params: {
+      page,
+      size,
+    },
+  });
 }
