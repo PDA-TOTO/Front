7f1d4c252d620ce647f5bdc5332b68ed1c8968cf
정희수 <heesoo@jeonghuisuui-MacBookPro.local>
2024-03-31 21:04:51 +0900

    feat : portfolio 조회

--- src/components/stockDetail/StockTradingBody.tsx
+++ src/components/stockDetail/StockTradingBody.tsx
@@ -8,6 +8,8 @@ import {
 } from "../../lib/hooks/stockReduxHooks";
 import AnimatedNumber from "../common/animate/AnimatedNumber";
 import { setPrice } from "../../store/reducers/stockControlReducers";
+import { useLocation } from "react-router-dom";
+import { getAllPortfolio } from "../../lib/apis/portfolios";
 
 type StockTradingBodyProps = {
   gap: "xs" | "sm" | "md" | "lg" | "xl";
@@ -28,15 +30,24 @@ const StockTradingBody: React.FC<StockTradingBodyProps> = ({
   gap,
   tradingType,
 }: StockTradingBodyProps) => {
+  const location = useLocation();
   const combobox = useCombobox({
     onDropdownClose: () => combobox.resetSelectedOption(),
   });
   const [targetPortfolio, setTargetPortfolio] = useState(portfolios[0]);
+  const [portList, setPortList] = useState([]);
   const stockDetailDispatch = useStockDetailDispatch();
   const stockControl = useStockDetailSelector((state) => state.stockControl);
   const stockWebSocket = useStockDetailSelector(
     (state) => state.stockWebSocket
   );
+
+  useEffect(() => {
+    getAllPortfolio().then((response) => {
+      console.log("response:", response.data.result);
+      setPortList(response.data.result);
+    });
+  }, []);
 
   useEffect(() => {
     stockDetailDispatch(setPrice(Number(stockWebSocket.bidp1)));
@@ -52,6 +63,11 @@ const StockTradingBody: React.FC<StockTradingBodyProps> = ({
     }
 
     return price * quantity;
+  };
+
+  const handleBuySell = () => {
+    const stockCode = location.pathname.split("/").pop();
+    console.log(stockControl.price, stockControl.quantity, stockCode);
   };
 
   return (
@@ -70,16 +86,19 @@ const StockTradingBody: React.FC<StockTradingBodyProps> = ({
           c="gray.5"
         >{`잔고: ${balance.toLocaleString()} 원`}</Text>
       </Stack>
-      <PortfolioComobobox
-        portfolios={portfolios}
-        targetPortfolio={targetPortfolio}
-        setTargetPortfolio={setTargetPortfolio}
-      />
+      {portList.length > 0 && (
+        <PortfolioComobobox
+          portfolios={portfolios}
+          targetPortfolio={targetPortfolio}
+          setTargetPortfolio={setTargetPortfolio}
+        />
+      )}
       <StockPriceInfobox />
       <Button
         color={tradingType === "BUY" ? "pink.5" : "secondary.5"}
         fullWidth
         autoContrast
+        onClick={() => handleBuySell()}
       >
         {tradingType === "BUY" ? "사기" : "팔기"}
       </Button>
