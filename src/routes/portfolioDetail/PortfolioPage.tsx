d9986dc2391eb6715ec33eeb3a8b74e025e9c079
jun <ryoojesu@naver.com>
2024-04-01 10:17:04 +0900

    myports 불러오기 수정 완

--- src/routes/portfolioDetail/PortfolioPage.tsx
+++ src/routes/portfolioDetail/PortfolioPage.tsx
@@ -13,7 +13,7 @@ type portfolioItem = {
     krxCode : {krxCode: string , name: string, type: string}
 }
 type myPort = {
-    weight: number[];
+    weight?: number[];
     id : number,
     ismain : boolean,
     portName : string,
@@ -33,12 +33,12 @@ const ScatterChartComponent: React.FC = (data) => {
             }}
         >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="x" type="number" name="beta" unit="" />
-            <YAxis dataKey="y" type="number" name="expected return" unit="%" />
-            <ZAxis dataKey="z" type="category" name="portName" unit="" />
+            <XAxis dataKey="x" type="number" name="베타" unit="" />
+            <YAxis dataKey="y" type="number" name="기대수익률" unit="%" />
+            <ZAxis dataKey="z" type="category" name="포트폴리오 이름" unit="" />
             <Tooltip cursor={{ strokeDasharray: '3 3' }} />
             <Legend />
-            <Scatter name="Portfolio" data={data} fill="#8884d8" />
+            <Scatter name="Portfolio" data={data} fill="#1639e8" />
         </ScatterChart>
     );
 };
@@ -50,27 +50,8 @@ const PortfolioPage: React.FC = () => {
     const [myPorts, setMyPorts] = useState<myPort[]>([])
     const [numSelected, setNumSelected] = useState<number>(0)
     const [betas, setBetas] = useState<Array<number[]>>([])
-    const [graph, setGraph ] = useState<{x : string, y : number, z : number}[]>([])
+    const [graph, setGraph ] = useState<{x : number, y : number, z : string}[]>([])
 
-    const data01 = [
-        { x: 1, y: 2  ,z : "port1"},
-        { x: 2, y: 5  ,z : "port2"},
-        { x: 3, y: 7  ,z : "port3"},
-        { x: 4, y: 9  ,z : "port4"},
-        { x: 5, y: 10 ,z : "port5"},
-    ];
-    useEffect(()=>{
-        const G = []
-        myPorts.map((elem: myPort, idx: number) => {
-            const temp: { x: number ; y: number; z: string  } = { x: 0, y: 0, z: '' }; // 타입 정의 수정
-            temp.z = elem.portName;
-            temp.x = Number(getBetaSum(myPorts,idx))
-            temp.y = Number(getER(myPorts,idx))
-            G.push(temp);
-          });
-        setGraph(G)
-        console.log(graph)
-    },[betas])
     useEffect(()=>{
         const func = async ()=>{
             const B = await getBeta();
@@ -79,23 +60,36 @@ const PortfolioPage: React.FC = () => {
         }
 
         func().then(result=>{
-            const port = result.data.result
-            port.map(async (elem : {id : number},idx : number)=>{
-                const res = await getWeight(elem.id)
-                const ratios : number[] = []
-                res.data.result.portfolioItems.map(async (elem : {ratio : number })=>{
-                    await ratios.push(elem.ratio)
-                })
-                const tempmyports = port
-                tempmyports[idx].weight = ratios
-            })
+            const port:myPort[] = result.data.result
+            console.log(port)
 
-            // console.log(port)
-            setMyPorts(port)
-            const names = port.map((elem: {portName : string})=>{return elem.portName})
-            setPortNames(names)
-        }).then();
+            const portWithRatios = port.map(async (elem)=>{
+                const res = await getWeight(elem.id)
+                
+                const weight: number[] = res.data.result.portfolioItems.map((elem : {ratio : number })=>{
+                    return elem.ratio
+                })
+                return {
+                    ...elem,
+                    weight
+                };
+            })
+            Promise.all(portWithRatios).then(value=>{
+                setMyPorts(value)
+            })
+        })
     },[])
+
+    useEffect(()=>{
+        const G : {x : number, y : number, z : string}[] = myPorts.map((elem: myPort, idx: number) => {
+            const temp: { x: number ; y: number; z: string  } = { x: 0, y: 0, z: '' }; // 타입 정의 수정
+            temp.z = elem.portName;
+            temp.x = Number(getBetaSum(myPorts,idx))
+            temp.y = Number(getER(myPorts,idx))
+            return temp
+        });
+        setGraph(G)
+    },[myPorts])
 
     function showDetail(myports : myPort[] , numSelected : number){
         // console.log("Myports ",myPorts)
@@ -113,12 +107,12 @@ const PortfolioPage: React.FC = () => {
                 
                 </div>
                 {selectedPorts?.map((elem : portfolioItem, idx : number)=>{
-                    return <div style={{height : "50px", border:"solid black 1px", margin : "20px", display:"flex", textAlign : "center"}}>
+                    return <div key={elem.id} style={{height : "50px", border:"solid black 1px", margin : "20px", display:"flex", textAlign : "center"}}>
                         <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{elem.krxCode.name}</div>
                         <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{elem.amount}</div>
-                        <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{elem.avg}</div>
-                        <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{elem.avg* elem.amount}</div>
-                        <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{myports[numSelected]?.weight?.[idx]?.toFixed(2)}</div>
+                        <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{elem.avg.toLocaleString()}원</div>
+                        <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{(elem.avg* elem.amount).toLocaleString()}원</div>
+                        <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{myports[numSelected]?.weight?.[idx]?.toFixed(2).toLocaleString()}</div>
                         <div style={{width : "200px", alignContent : "center", alignItems:"center "}}>{betas[numSelected][idx].toFixed(2)}</div>
                         </div>})}
             </>
@@ -142,7 +136,7 @@ const PortfolioPage: React.FC = () => {
             const beta =  betas[numSelected]?.[idx]
             const weight = myPorts[numSelected]?.weight?.[idx] 
             // console.log(beta,weight)
-            sum += beta * weight
+            sum += beta * weight!
         })
         return (sum/100).toFixed(2);
     }
@@ -159,7 +153,7 @@ const PortfolioPage: React.FC = () => {
         <Grid grow justify="space-between" px={{ base: 72 }} pt={34} style={{display:"flex"}}>
             <Grid.Col span={4}>
                 <h2>
-                    포트폴리오 지표 분석
+                    포트폴리오 지표 분석(베타 - 기대수익률)
                 </h2>
                 <div style={{ width: '100%', height: 300 }}>
                     {/* <ScatterChartComponent  /> */}
@@ -172,7 +166,7 @@ const PortfolioPage: React.FC = () => {
                     </div>
                     <select style={{marginLeft:"10px"}} value={numSelected} onChange={(e)=>{setNumSelected(Number(e.target.value))}}>
                         {/* {portNames.map((elem,idx)=>{return <option value={idx}>{elem}</option>})} */}
-                        {myPorts.map((elem : {portName : string},idx)=>{return <option value={idx}>{elem.portName}</option>})}
+                        {myPorts.map((elem,idx)=>{return <option key={elem.id} value={idx}>{elem.portName}</option>})}
                     </select>
                     {/* <Button style={{marginLeft:"10px"}} onClick={openDeleteModal}>포트폴리오 삭제하기</Button> */}
                     <button style={{marginLeft:"10px"}} onClick={(e)=>{
@@ -190,10 +184,10 @@ const PortfolioPage: React.FC = () => {
                 </div>
 
                 <div style={{display: "flex", alignItems: "center"}}>
-                    <h1 style={{marginLeft : "10px" ,fontFamily : "Arial",width : "200PX",fontWeight : 'bold' ,display : "flex"}}>{myPorts[numSelected]?.portName}</h1>
-                    <h3 style={{marginLeft : "50px"}}>베타 : {getBetaSum(myPorts,numSelected)}</h3>
+                    <h1 style={{marginLeft : "10px" ,fontFamily : "Arial",width : "400px",fontWeight : 'bold' ,display : "flex"}}>{myPorts[numSelected]?.portName}</h1>
                     <h3 style={{marginLeft :"30px" , fontWeight : 'bold' ,display : "flex"}}>
-                        포트폴리오 자산 : {getPortSum(myPorts,numSelected)}</h3>
+                        포트폴리오 자산 : {getPortSum(myPorts,numSelected).toLocaleString()}원</h3>
+                    <h3 style={{marginLeft : "50px"}}>베타 : {getBetaSum(myPorts,numSelected)}</h3>
                     <h3 style={{marginLeft :"30px" , fontWeight : 'bold' ,display : "flex"}}>기대수익률 : {getER(myPorts,numSelected)}%</h3>    
                 </div>
                 {showDetail(myPorts, numSelected)}      
